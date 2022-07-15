c82e12403b811dcf929f606d8e1ba714443f1b97
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-15 12:52:18 -0400

    Add Cord

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 app.use(express.json());
 
+// CORS Added to allow request from specific origin
+const cors = require('cors');
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 // Example use route and controller for the first two methods
 require("./routes/product.routes")(app);
 // Routes Customers
