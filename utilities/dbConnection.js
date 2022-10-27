d736d0e8e098afeb0d2fcad01d4baa947585ef36
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-10-27 18:47:42 -0400

    Add mysql2

--- utilities/dbConnection.js
+++ utilities/dbConnection.js
@@ -3,6 +3,7 @@ const sql = require("mssql/msnodesqlv8");
 // My Sql
 const mysql = require("mysql")
 
+
 function getConnection() {
   try {
     return new sql.ConnectionPool({
@@ -19,27 +20,12 @@ function getConnection() {
 }
 
 // Local MySQL CONNECTION
-// function getMySqlConnection() {
-//   try {
-//     return new mysql.createPool({
-//       connectionLimit : 100, //important
-//       database: "Invoices",
-//       user:'root',
-//       password: '49bcrfem',
-//       host: "localhost",
-//       debug:false,
-//     });
-//   } catch (error) {
-//     console.log("Error get MySql connectinString");
-//   }
-// }
-
 function getMySqlConnection() {
   try {
     return new mysql.createPool({
       connectionLimit : 100, //important
-      database: "smarxihn_invoice_db",
-      user:'smarxihn_invoice_use',
+      database: "Invoices",
+      user:'root',
       password: '49bcrfem',
       host: "localhost",
       debug:false,
@@ -48,6 +34,21 @@ function getMySqlConnection() {
     console.log("Error get MySql connectinString");
   }
 }
+
+// function getMySqlConnection() {
+//   try {
+//     return new mysql.createPool({
+//       connectionLimit : 100, //important
+//       database: "smarxihn_invoice_db",
+//       user:'smarxihn_invoice_use',
+//       password: '49bcrfem',
+//       host: "localhost",
+//       debug:false,
+//     });
+//   } catch (error) {
+//     console.log("Error get MySql connectinString");
+//   }
+// }
 
 // module.exports.getConnection = getConnection;
 // module.exports.getMySqlConnection = getMySqlConnection;
